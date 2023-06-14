c6fcb96e9bd1145ac2b30a18335cf09552248a52
Mac-ziis <macg286@gmail.com>
2023-06-13 19:02:12 -0700

    Add the eventListener to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-function getAndSetCries () {
+function getAndSetCries (event) {
+  event.preventDefault();
   const criesInput = document.getElementById("criesInput").value;
 
   document.querySelector("span#cries").innerText = criesInput;
   document.querySelector("span#cries2").innerText = criesInput;
+  document.querySelector('div#story').removeAttribute('class');
 }
 //need to bring the return to uppercase
 
 window.onload = function() {
   let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    getAndSetCries();
-    document.querySelector("div#story").removeAttribute("class");
-  };
+  form.addEventListener('submit', getAndSetCries);
+
+  // form.onsubmit = function(event) {
+    // event.preventDefault();
+    // getAndSetCries();
+    // document.querySelector("div#story").removeAttribute("class");
 };
